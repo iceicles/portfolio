7857458108e1a19447c1dfa148f86608976086b1
oshiogifted <oshiogifted2011@gmail.com>
2024-11-07 00:00:11 -0500

    responsive styles for footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -9,11 +9,11 @@ const Footer: FC<{}> = ({}) => {
         <footer>
           <div className='w-full p-4 md:py-8'>
             <hr className='my-6 sm:mx-auto lg:my-8' />
-            <div className='sm:flex sm:items-center sm:justify-between'>
+            <div className='sm:flex lg:items-center sm:justify-between'>
               <span>MU (logo)</span>
-              <ul className='flex flex-wrap items-center'>
+              <ul className='sm:flex md:flex-row sm:flex-wrap sm:items-center flex flex-col gap-1'>
                 {FooterNavs.map((footer) => (
-                  <li key={footer.id}>
+                  <li key={footer.id} className='self-end lg:self-auto p-2'>
                     <Link
                       href={footer.href}
                       className='hover:underline me-4 md:me-6'
@@ -25,7 +25,7 @@ const Footer: FC<{}> = ({}) => {
               </ul>
             </div>
 
-            <div className='block text-sm sm:text-center'>
+            <div className='block text-sm lg:text-center mt-4'>
               © 2024 ❤ Hand made by {}
               <Link
                 href='https://github.com/iceicles'
