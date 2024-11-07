9e6f60fe9d17e68654050fe7a16230ad205b3a38
oshiogifted <oshiogifted2011@gmail.com>
2024-11-07 18:39:54 -0500

    moved logo into header nav

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -9,13 +9,15 @@ const Header: FC<{}> = ({}) => {
       <div className='flex align-element justify-center py-4 sm:gap-x-16 sm:items-center sm:py-8 relative mb-[10vh]'>
         <nav>
           <ul className='flex gap-10'>
-            {HeaderNavs.map((el, _i) => (
-              <HeaderNav key={_i} value={el.value} href={el.href} />
-            ))}
+            <>
+              {HeaderNavs.map((el, _i) => (
+                <HeaderNav key={_i} value={el.value} href={el.href} />
+              ))}
+              <li className='absolute right-[15%]'>
+                <Link href='/'>MU</Link>
+              </li>
+            </>
           </ul>
-          <h1 className='absolute right-[15%]'>
-            <Link href='/'>MU</Link>
-          </h1>
         </nav>
       </div>
     </>
