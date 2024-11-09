78fbea969f2eb911a41b187d11fe5b07294f6163
oshiogifted <oshiogifted2011@gmail.com>
2024-11-09 00:55:36 -0500

    style changes to initial logo in mobile burger nav

--- components/Navigation/HeaderNav/MobileHamburgerNav/index.tsx
+++ components/Navigation/HeaderNav/MobileHamburgerNav/index.tsx
@@ -9,19 +9,19 @@ export const MobileHamburgerNav: FC<{}> = ({}) => {
 
   return (
     <>
-      <div className='fixed top-0 left-0 p-4'>
-        <InitialsLogo />
+      <div className='flex justify-between items center w-full'>
+        <InitialsLogo styles='self-center' />
+        <label className='hamburger z-10'>
+          <input type='checkbox' onClick={() => setToggleModal(!toggleModal)} />
+          <svg viewBox='0 0 32 32'>
+            <path
+              className='line line-top-bottom'
+              d='M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22'
+            ></path>
+            <path className='line' d='M7 16 27 16'></path>
+          </svg>
+        </label>
       </div>
-      <label className='hamburger z-10'>
-        <input type='checkbox' onClick={() => setToggleModal(!toggleModal)} />
-        <svg viewBox='0 0 32 32'>
-          <path
-            className='line line-top-bottom'
-            d='M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22'
-          ></path>
-          <path className='line' d='M7 16 27 16'></path>
-        </svg>
-      </label>
 
       {toggleModal && (
         <>
