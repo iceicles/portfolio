147f8a792aede4dd7ddb2845b7aeafa00243ebe4
oshiogifted <oshiogifted2011@gmail.com>
2024-11-09 01:56:53 -0500

    added animation to github name in footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -31,11 +31,11 @@ const Footer: FC<{}> = ({}) => {
               <Link
                 href='https://github.com/iceicles'
                 target='_blank'
-                className='hover:underline hover:decoration-gray-800 hover:decoration-4 relative group'
+                className='hover:underline hover:decoration-gray-800 hover:decoration-4 relative group animate-pulse'
               >
                 Iceicles{' '}
                 <span className='absolute left-[3rem] group-hover:no-underline'>
-                  ❤
+                  <span className='text-red-800'>❤️</span>
                 </span>
                 <span className='absolute hidden group-hover:block bg-gray-800 rounded-md px-2 py-1 top-0 left-[145%] w-max'>
                   My Github
