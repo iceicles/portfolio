3c1384c1b1e498a305acc202db14da7f584ba33d
oshiogifted <oshiogifted2011@gmail.com>
2024-11-10 23:51:45 -0500

    removed top margin from homepage hero section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from '../containers/home-page/hero-section';
 export default function Home() {
   return (
     <>
-      <main className='m-auto w-[75%] mt-[10vh]'>
+      <main className='m-auto w-[75%]'>
         <HeroSection />
       </main>
     </>
