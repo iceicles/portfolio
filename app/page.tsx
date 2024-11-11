2b6e6e0a42d5d6a0e74b03e6be5ba5a2d20f7f17
oshiogifted <oshiogifted2011@gmail.com>
2024-11-11 18:38:58 -0500

    width changes to homepage hero section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from '../containers/home-page/hero-section';
 export default function Home() {
   return (
     <>
-      <main className='m-auto w-[75%]'>
+      <main className='m-auto w-[85%] md:w-[75%]'>
         <HeroSection />
       </main>
     </>
