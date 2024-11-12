80594977dbd586a3a9baa2f0758b2476411a3fae
oshiogifted <oshiogifted2011@gmail.com>
2024-11-11 20:38:50 -0500

    changes to initials logo

--- components/Logo/InitialsLogo/index.tsx
+++ components/Logo/InitialsLogo/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import { InitialsLogo as Logo } from '../../Icons';
 
 interface initialsLogo {
   styles?: string;
@@ -11,7 +12,9 @@ export const InitialsLogo: FC<initialsLogo> = ({ styles, value = 'MU' }) => {
     <>
       <strong className={styles}>
         <Link href='/'>
-          <div className='hover:animate-logo-spin text-2xl'>{value}</div>
+          <div className='hover:animate-logo-spin text-2xl'>
+            <Logo className='text-6xl h-6' />
+          </div>
         </Link>
       </strong>
     </>
