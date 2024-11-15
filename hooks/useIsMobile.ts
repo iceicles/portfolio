df80b8d18fd235a2998972b2fa814ee90471dea9
oshiogifted <oshiogifted2011@gmail.com>
2024-11-14 20:42:04 -0500

    fixed useIsMobile hook rendering hamburger on page reload in desktop (tip: use boolean in useState)

--- hooks/useIsMobile.ts
+++ hooks/useIsMobile.ts
@@ -1,28 +1,36 @@
-'use client'
-import { useState, useEffect } from "react"
-import { BREAKPOINTS } from "../global_constants/config"
+'use client';
+import { useState, useEffect } from 'react';
+import { BREAKPOINTS } from '../global_constants/config';
 
 /*
-* Hook to check the current window width against the mobile max width (767)
-* @return {boolean} true | false
-*/
+ * Hook to check the current window width against the mobile max width (767)
+ * @return {boolean} true | false
+ */
 
+// hook to detect mobile viewport size
 export const useIsMobile = () => {
+  const breakpoint = BREAKPOINTS.mobileMaxWidth;
+  const [isMobile, setIsMobile] = useState(false);
 
-    const breakpoint: number = BREAKPOINTS.mobileMaxWidth
-    const [windowWidth, setWindowInnerWidth] = useState<number>(breakpoint)
+  useEffect(() => {
+    // function to check if the viewport width is below the breakpoint
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth <= breakpoint);
+    };
 
-    const resizeWindow = () => {
-        setWindowInnerWidth(window.innerWidth)
-    }
+    // initial check on mount
+    checkIsMobile();
 
-    useEffect(() => {
-        resizeWindow()
-        window.addEventListener('resize', resizeWindow)
+    // event listener for window resize to update the state
+    window.addEventListener('resize', checkIsMobile);
 
-        return () => window.removeEventListener('resize', resizeWindow)
-       
-    }, [resizeWindow])
+    // clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', checkIsMobile);
+    };
+  }, [breakpoint]); // only re-run effect if breakpoint changes
 
-    return windowWidth <= breakpoint
-}
+  return isMobile;
+};
+
+export default useIsMobile;
