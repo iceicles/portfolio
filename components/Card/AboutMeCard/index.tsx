7c006bef391140cd91431ec1a6e86af59b599323
oshiogifted <oshiogifted2011@gmail.com>
2024-11-14 21:09:13 -0500

    file not needed - to be deleted

--- components/Card/AboutMeCard/index.tsx
+++ components/Card/AboutMeCard/index.tsx
@@ -1,65 +1,67 @@
-import Link from 'next/link';
-import React, { FC } from 'react';
+// file not in use
 
-interface CardProps {
-  id?: number;
-  isCurrent?: boolean;
-  title: string;
-  href: string;
-  description: string;
-  workplace: string;
-  yearsActive: string;
-  isTools?: boolean;
-  tools?: Array<string>;
-}
+// import Link from 'next/link';
+// import React, { FC } from 'react';
 
-const AboutMeCard: FC<CardProps> = ({
-  isCurrent,
-  title,
-  href,
-  description,
-  workplace,
-  yearsActive,
-  isTools,
-  tools,
-}) => {
-  const borderClass = 'border-white border-2 border-solid rounded';
-  const tagBorderClass = 'border-indigo-500 border-2 border-dotted rounded';
-  return (
-    <span className='w-[40%] relative'>
-      <div className={`h-[100%] w-[100%] p-[10%] ${borderClass}`}>
-        {isCurrent && (
-          <p className='absolute top-[10%] w-fit h-fit -rotate-45'>Current</p>
-        )}
-        <div className='flex flex-col items-center'>
-          <h2 className='text-4xl'>{title}</h2>
-          <div className='flex justify-between gap-[5%] self-end mt-[10%] text-center w-[200px]'>
-            <Link href={href} target='_blank' className={tagBorderClass}>
-              {workplace}
-            </Link>
-            <span className={tagBorderClass}>{yearsActive}</span>
-          </div>
-        </div>
-        <div className='flex justify-between mt-[10%]'>
-          <p>{description}</p>
-          {isTools && (
-            <div>
-              <h3 className='border-b-2 w-max'>
-                Tools / Architectural Patterns
-              </h3>
-              <div className='border-l-2'>
-                <ul className=' list-[square] ml-[20%]'>
-                  {tools?.map((tool, _i) => (
-                    <li key={_i}>{tool}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </span>
-  );
-};
+// interface CardProps {
+//   id?: number;
+//   isCurrent?: boolean;
+//   title: string;
+//   href: string;
+//   description: string;
+//   workplace: string;
+//   yearsActive: string;
+//   isTools?: boolean;
+//   tools?: Array<string>;
+// }
 
-export default AboutMeCard;
+// const AboutMeCard: FC<CardProps> = ({
+//   isCurrent,
+//   title,
+//   href,
+//   description,
+//   workplace,
+//   yearsActive,
+//   isTools,
+//   tools,
+// }) => {
+//   const borderClass = 'border-white border-2 border-solid rounded';
+//   const tagBorderClass = 'border-indigo-500 border-2 border-dotted rounded';
+//   return (
+//     <span className='w-[40%] relative'>
+//       <div className={`h-[100%] w-[100%] p-[10%] ${borderClass}`}>
+//         {isCurrent && (
+//           <p className='absolute top-[10%] w-fit h-fit -rotate-45'>Current</p>
+//         )}
+//         <div className='flex flex-col items-center'>
+//           <h2 className='text-4xl'>{title}</h2>
+//           <div className='flex justify-between gap-[5%] self-end mt-[10%] text-center w-[200px]'>
+//             <Link href={href} target='_blank' className={tagBorderClass}>
+//               {workplace}
+//             </Link>
+//             <span className={tagBorderClass}>{yearsActive}</span>
+//           </div>
+//         </div>
+//         <div className='flex justify-between mt-[10%]'>
+//           <p>{description}</p>
+//           {isTools && (
+//             <div>
+//               <h3 className='border-b-2 w-max'>
+//                 Tools / Architectural Patterns
+//               </h3>
+//               <div className='border-l-2'>
+//                 <ul className=' list-[square] ml-[20%]'>
+//                   {tools?.map((tool, _i) => (
+//                     <li key={_i}>{tool}</li>
+//                   ))}
+//                 </ul>
+//               </div>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </span>
+//   );
+// };
+
+// export default AboutMeCard;
