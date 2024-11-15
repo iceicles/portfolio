c02a4ee49a1af7c40f309ba2219d986ce1774bf1
oshiogifted <oshiogifted2011@gmail.com>
2024-11-14 22:35:16 -0500

    deleted unused pages

--- app/memory-dump/page.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-
-const MemoryDump: FC<{}> = ({}) => {
-  return <div>Place for shitpostings/blogs/memes etc...</div>;
-};
-
-export default MemoryDump;
--- app/tools/page.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-
-const Tools: FC<{}> = ({}) => {
-  return <h1>Tools I use</h1>;
-};
-
-export default Tools;
