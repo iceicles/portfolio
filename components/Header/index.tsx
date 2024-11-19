9f6f50486330b31692a62fc8980e12780eb18c5d
oshiogifted <oshiogifted2011@gmail.com>
2024-11-19 00:11:13 -0500

    todo: fix tablet/desktop header nav flicker

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import { HeaderNavs } from './constants';
 import { HeaderNav } from '../Navigation/HeaderNav';
 import { useIsMobile } from '../../hooks/useIsMobile';
@@ -7,11 +7,28 @@ import { MobileHamburgerNav } from '../Navigation/HeaderNav/MobileHamburgerNav';
 import { InitialsLogo } from '../Logo/InitialsLogo';
 import { isActive } from '../../utils/isActive';
 import { usePathname } from 'next/navigation';
+import { BREAKPOINTS } from '../../global_constants/config';
 
 const Header: FC<{}> = ({}) => {
   const isMobile = useIsMobile();
 
   const pathname = usePathname();
+  // TODO - fix tablet/desktop header nav displaying when refresh in mobile view
+  // below doesn't really work unless you remove typeof window!== 'undefined' but
+  // nextjs will complain...
+
+  // Instantly renders the mobile nav header - fixes flicker where we see desktop header then mobile
+  // can return a loading spinner (although why would you do that here)
+  // if (
+  //   isMobile === null &&
+  //   typeof window !== 'undefined' &&
+  //   window.innerWidth < BREAKPOINTS.tabletWidth
+  // ) {
+  //   return (
+  //     // <div className='sm:py-8 relative mb-[8vh] desktop-lg:mb-[15vh]'></div>
+  //     <MobileHamburgerNav />
+  //   );
+  // }
 
   return (
     <>
@@ -21,23 +38,21 @@ const Header: FC<{}> = ({}) => {
         <div className='flex justify-center items-center py-4 sm:gap-x-16 sm:py-8 relative mb-[8vh] desktop-lg:mb-[15vh]'>
           <nav>
             <ul className='flex gap-10'>
-              <>
-                {HeaderNavs.map((el, _i) => (
-                  <HeaderNav
-                    key={_i}
-                    value={el.value}
-                    href={el.href}
-                    styles={
-                      isActive(el.href, pathname)
-                        ? 'active-link font-bold'
-                        : 'hover:hover-link'
-                    }
-                  />
-                ))}
-                <li className='absolute left-[2%]'>
-                  <InitialsLogo />
-                </li>
-              </>
+              {HeaderNavs.map((el, _i) => (
+                <HeaderNav
+                  key={_i}
+                  value={el.value}
+                  href={el.href}
+                  styles={
+                    isActive(el.href, pathname)
+                      ? 'active-link font-bold'
+                      : 'hover:hover-link'
+                  }
+                />
+              ))}
+              <li className='absolute left-[2%]'>
+                <InitialsLogo />
+              </li>
             </ul>
           </nav>
         </div>
