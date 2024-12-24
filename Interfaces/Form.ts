d4d0d4af670a9d288c35c72253803424db2153a5
oshiogifted <oshiogifted2011@gmail.com>
2024-12-24 13:32:29 -0500

    form interface folder name update

--- Interfaces/Form.ts
+++ Interfaces/Form.ts
@@ -1,14 +1,14 @@
 // contains interfaces shared in multiple components
 
 export interface IFormValues {
-  name: string;
-  email: string;
-  message: string;
-  required: boolean;
-}
-
-export interface IFormErrorInputs {
-  nameError: string | undefined;
-  emailError: string | undefined;
-  messageError: string | undefined;
-}
+    name: string;
+    email: string;
+    message: string;
+    required: boolean;
+  }
+  
+  export interface IFormErrorInputs {
+    nameError: string | undefined;
+    emailError: string | undefined;
+    messageError: string | undefined;
+  }
