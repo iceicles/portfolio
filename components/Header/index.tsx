efb17c0d8578cb1172c6b6e2cdc72d0617d1d94f
oshiogifted <oshiogifted2011@gmail.com>
2024-12-26 16:47:50 -0500

    fixed tablet/desktop header nav displaying upon refresh in mobile view

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -7,35 +7,18 @@ import { MobileHamburgerNav } from '../Navigation/HeaderNav/MobileHamburgerNav';
 import { InitialsLogo } from '../Logo/InitialsLogo';
 import { isActive } from '../../utils/isActive';
 import { usePathname } from 'next/navigation';
-import { BREAKPOINTS } from '../../global_constants/config';
 
 const Header: FC<{}> = ({}) => {
   const isMobile = useIsMobile();
 
   const pathname = usePathname();
-  // TODO - fix tablet/desktop header nav displaying when refresh in mobile view
-  // below doesn't really work unless you remove typeof window!== 'undefined' but
-  // nextjs will complain...
-
-  // Instantly renders the mobile nav header - fixes flicker where we see desktop header then mobile
-  // can return a loading spinner (although why would you do that here)
-  // if (
-  //   isMobile === null &&
-  //   typeof window !== 'undefined' &&
-  //   window.innerWidth < BREAKPOINTS.tabletWidth
-  // ) {
-  //   return (
-  //     // <div className='sm:py-8 relative mb-[8vh] desktop-lg:mb-[15vh]'></div>
-  //     <MobileHamburgerNav />
-  //   );
-  // }
 
   return (
     <>
       {isMobile ? (
         <MobileHamburgerNav />
       ) : (
-        <div className='flex justify-center items-center py-4 sm:gap-x-16 sm:py-8 relative mb-[8vh] desktop-lg:mb-[15vh]'>
+        <div className='md:flex justify-center items-center py-4 sm:gap-x-16 sm:py-8 relative mb-[8vh] desktop-lg:mb-[15vh] invisible md:visible'>
           <nav>
             <ul className='flex gap-10'>
               {HeaderNavs.map((el, _i) => (
