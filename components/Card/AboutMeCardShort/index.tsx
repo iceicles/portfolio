5c098332bdbcd11da32c896011425dd0688a0a62
oshiogifted <oshiogifted2011@gmail.com>
2024-12-27 22:15:43 -0500

    style updates

--- components/Card/AboutMeCardShort/index.tsx
+++ components/Card/AboutMeCardShort/index.tsx
@@ -18,12 +18,12 @@ const AboutMeCardShort: FC<AboutMeCardShort> = ({
     <>
       <button
         onClick={() => onClick(id)}
-        className='max-lg:border-button p-2 rounded-xl shadow-2xl shadow-blue-500/30 md:hover:border-button hover:animate-pulse2 border border-solid lg:border-none'
+        className='max-lg:border-button p-[14px] md:p-4 rounded-xl shadow-2xl shadow-blue-500/30 md:hover:border-button hover:animate-pulse2 border border-solid lg:border-none'
       >
         <h1 className='text-xl'>{title}</h1>
-        <span className='flex gap-1 justify-center items-center'>
+        {/* <span className='flex gap-1 justify-center items-center'>
           <CTCLogo /> <p>{workplace}</p>{' '}
-        </span>
+        </span> */}
       </button>
     </>
   );
--- containers/about-page/timeline-section/index.tsx
+++ containers/about-page/timeline-section/index.tsx
@@ -8,17 +8,24 @@ interface TimelineSection {
 
 const TimelineSection: FC<TimelineSection> = ({ showModal }) => {
   return (
-    <section className='flex flex-col sm:flex-row lg:flex-col mx-12 mt-12 sm:m-auto sm:mt-12 gap-4 md:gap-[5vw] justify-evenly sm:flex-wrap'>
-      {AboutMeCards.map((card) => (
-        <React.Fragment key={card.id}>
-          <AboutMeCardShort
-            id={card.id}
-            title={card.title}
-            workplace={card.workplace}
-            onClick={() => showModal(card.id)}
-          />
-        </React.Fragment>
-      ))}
+    <section className='flex flex-col sm:flex-row lg:flex-col mt-12 sm:m-auto sm:mt-12 gap-4 md:gap-[5vw] justify-evenly sm:flex-wrap xl:border-l-2'>
+      <div className='flex flex-col gap-16 mx-12'>
+        <h1 className='text-3xl text-center text-8 font-medium underline underline-offset-8 decoration-wavy decoration-blue-500/40 xl:no-underline'>
+          <i>Experiences</i>
+        </h1>
+        <div className='flex flex-col sm:flex-row lg:flex-col gap-5'>
+          {AboutMeCards.map((card) => (
+            <React.Fragment key={card.id}>
+              <AboutMeCardShort
+                id={card.id}
+                title={card.title}
+                workplace={card.workplace}
+                onClick={() => showModal(card.id)}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
